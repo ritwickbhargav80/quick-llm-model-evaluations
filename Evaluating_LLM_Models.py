5d43f81744d2437b39aa8cb202371c4b6aa17f2b
Ritwick Bhargav <ritwickbhargav80@gmail.com>
2024-08-24 23:51:01 +0530

    --fix: minor workaround for streamlit deployment!

--- Evaluating_LLM_Models.py
+++ Evaluating_LLM_Models.py
@@ -18,6 +18,15 @@ import time
 import validators
 import streamlit as st
 import streamlit.components.v1 as components
+import nltk
+
+nltk_data_dir = "./resources/nltk_data_dir/"
+if not os.path.exists(nltk_data_dir):
+    os.makedirs(nltk_data_dir, exist_ok=True)
+nltk.data.path.clear()
+nltk.data.path.append(nltk_data_dir)
+nltk.download('punkt', download_dir=nltk_data_dir)
+
 from beyondllm import source, retrieve, embeddings, llms, generator
 
 
