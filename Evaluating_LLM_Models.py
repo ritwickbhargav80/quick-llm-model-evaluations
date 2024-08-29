12355fe26323a5cd1445779ecc027959148e4e86
Ritwick Bhargav <ritwickbhargav80@gmail.com>
2024-08-29 15:26:19 +0000

    --update: Multiple LLMs evaluation simultaneously!

--- Evaluating_LLM_Models.py
+++ Evaluating_LLM_Models.py
@@ -1,280 +1,135 @@
-# This script requires the following external libraries:
-# - beyondllm
-# - validators
-# - llama-index-readers-web
-# - youtube_transcript_api
-# - llama-index-readers-youtube-transcript
-# - llama-index-embeddings-azure_openai
-# - anthropic
-# - llama-index-embeddings-fastembed
-# - docx2txt
-
-# Install them using pip:
-#   pip install beyondllm validators llama-index-readers-web youtube_transcript_api llama-index-readers-youtube-transcript llama-index-embeddings-azure_openai anthropic llama-index-embeddings-fastembed docx2txt
-
-import os
-import re
-import time
-import validators
 import streamlit as st
-import streamlit.components.v1 as components
-import nltk
-
-nltk_data_dir = "./resources/nltk_data_dir/"
-if not os.path.exists(nltk_data_dir):
-    os.makedirs(nltk_data_dir, exist_ok=True)
-nltk.data.path.clear()
-nltk.data.path.append(nltk_data_dir)
-nltk.download('punkt', download_dir=nltk_data_dir)
-
-from beyondllm import source, retrieve, embeddings, llms, generator
-
-
-def format_rag_triad_evals(evals: str) -> dict:
-    """
-    Parses the raw RAG triad evaluation string into a well-structured dictionary 
-    containing context_relevancy, answer_relevancy, and groundness scores.
-
-    Parameters
-    ----------
-        evals (str): The raw evaluation string output by the pipeline.
-
-    Returns
-    ----------
-        dict: A dictionary containing the formatted evaluation scores.
-    """
-    lower_evals = str(evals).lower()
-    result = {
-        "context_relevancy": float(lower_evals.split("context relevancy score: ")[1].split("\n")[0]),
-        "answer_relevancy": float(lower_evals.split("answer relevancy score: ")[1].split("\n")[0]),
-        "groundness": float(lower_evals.split("groundness score: ")[1].split("\n")[0]),
-    }
-    return result
-
-
-def metric_custom_css() -> None:
-    """
-    Modifying CSS of evaluation metrics displayed in st.metric based on their dynamic values.
-    Injects the CSS code into the Streamlit app using the components.html function.
-
-    Parameters
-    ----------
-        None
-    Returns
-    ----------
-        None
-    """
-    
-    html_string = """
-        <script>
-            const metricLabels = window.parent.document.querySelectorAll("[data-testid='stMetricLabel']");
-            for (const metricLabel of metricLabels)
-            {
-                metricLabel.style.width = "fit-content";
-                metricLabel.style.margin = "auto";
-            }
-
-            const metricValues = window.parent.document.querySelectorAll("[data-testid='stMetricValue']");
-            for (const metricValue of metricValues)
-            {
-                const value = parseFloat(metricValue.textContent);
-                let color = ((value <= 3) ? "red" : ((value <= 6) ? "yellow" : "green"));
-                metricValue.style.color = color;
-                metricValue.style.textAlign = "center";
-            }
-        </script>
-        """
-    components.html(f"{html_string}")
-
-# Mapping LLMs to available models
-llm_to_model_tag = {
-    "Gemini": ("gemini-1.0-pro", "gemini-pro", "gemini-1.5-pro-latest"),
-    "OpenAI": ("gpt-3.5-turbo", "gpt-4", "gpt-4-turbo"),
-    "Azure OpenAI": ("gpt-35-turbo", "gpt-4", "gpt-35-turbo-16k"),
-    "Anthropic": ("claude-3-5-sonnet-20240620", "claude-3-haiku-20240307", "claude-3-sonnet-20240229", "claude-3-opus-20240229")
-}
-
-# Sidebar configuration section
-st.sidebar.title("Configure:")
-
-# LLM selection dropdown
-llm_selected = st.sidebar.selectbox(label="Which LLM you want to use?", options=llm_to_model_tag.keys())
-
-if(llm_selected=="Azure OpenAI"):
-    azure_oai_endpoint = st.sidebar.text_input(label="What's your Azure OpenAI Endpoint URL?", placeholder="https://<azure-openai-resource-name>.openai.azure.com/")
-    
-    # URL validation check
-    if (azure_oai_endpoint.strip() != "") and (not bool(re.match("https:\/\/(.+)\.openai\.azure\.com[/]{0,1}", azure_oai_endpoint))):
-        st.sidebar.error("Please enter a valid Endpoint!")
-
-# API key input for selected LLM
-api_key = st.sidebar.text_input(label=f"Provide your API Key{(' for '+ str(llm_selected)) if llm_selected else ''}:", type="password")
-
-# Model selection dropdown based on chosen LLM
-model_name = st.sidebar.selectbox(label=f"Which model from {llm_selected} you want to use?", options=llm_to_model_tag[llm_selected])
-
-if(llm_selected == "Azure OpenAI"):
-    # Configuration for Azure OpenAI LLM
-    azure_oai_deployment_name = st.sidebar.text_input(label=f"Deployment name for {model_name}?", value=model_name)
-    embeddings_oai_model_name = st.sidebar.text_input(label="Embeddings model name?", value="text-embedding-ada-002")
-elif(llm_selected == "Anthropic"):
-    # Information for Anthropic LLM
-    st.sidebar.warning("We use FastEmbedEmbedding('BAAI/bge-small-en-v1.5') model from llama_index for embeddings!")
-
-# Warning message if no models available for chosen LLM
-if llm_to_model_tag[llm_selected] == ():
-    st.sidebar.warning("Uh-ohh! Models are getting added soon..")
-
-# Source selection and content for RAG in columns layout
-source_column, second_column = st.sidebar.columns(2)
-source_selected = source_column.selectbox(label="Which source?", options=("URL", "YouTube", "PDF", "DOCX"))
-
-if(source_selected in ("URL", "YouTube")):
-    # Prompt the user for the content link in the second column based on user selection
-    url = second_column.text_input(
-        label="Link to your content:",
-        value=(
-            st.secrets["DEFAULTS"]["PAGE_URL"]
-            if (source_selected.lower() == "url")
-            else st.secrets["DEFAULTS"]["YOUTUBE_URL"]
-        ),
-    )
-
-    # URL validation check
-    if (url.strip() != "") and (not validators.url(url)):
-        st.sidebar.error("Please enter a valid URL!")
-else:
-    # If not URL or YouTube, allow file upload based on selected source
-    uploaded_file = st.sidebar.file_uploader(f"Choose a {source_selected.upper()} file", type=source_selected.lower())
 
 # Main content section
-st.title("Perform quick LLM evaulations🔄️")
+st.title("Perform quick LLM evaluations🔄️")
 
-# Custom CSS for better formatting
-st.markdown(
+st.write("Unlock the power of language models quickly and efficiently with our streamlined evaluation process. Whether you’re working with a single LLM or comparing multiple models, our platform makes it easy to assess Context Relevance, Answer Relevance, and Groundedness for your needs.")
+
+st.html("""
+<ul>
+  <li><strong>Quick and Simple:</strong> Perform rapid evaluations with a few clicks. No need for complex setups or lengthy processes.</li>
+  <li><strong>Single Model Assessments:</strong> Evaluate one language model at a time to get detailed insights for complete visibility.</li>
+  <li><strong>Multiple Model Comparisons:</strong> Simultaneously compare multiple LLMs to see how they stack up against each other, saving you time and effort.</li>
+</ul>
+""")
+
+st.write("Start your evaluations now and find the language model that fits your project perfectly.")
+
+st.header("Release Notes:")
+
+st.markdown("""
+  <style>
+    div[data-testid="stExpander"] > details > summary > span > div > p {
+      font-size: 1.1rem;
+    }
+  </style>"""
+, unsafe_allow_html=True)
+
+versions = [
+  {
+    "version_name": "v1.1.0",
+    "header": "Simultaneous Evaluation with Multiple LLMs Support",
+    "release_date": "August 29, 2024",
+    "markdown_content": """
+      This release introduces a major update to our Streamlit app, now featuring the capability to perform evaluations with multiple LLMs simultaneously. Users can now select and evaluate multiple models in one go, expanding the flexibility and depth of evaluations. 
+      
+      **:blue[<u>What's New:</u>]**
+        - <u>:rainbow[**Multiple LLM Evaluations Simultaneously**:]</u> :green[Evaluate multiple models in one go, providing a more comprehensive analysis.]
+        - <u>:rainbow[**Color Coding**:]</u> :green[Selected LLM models are now highlighted with background colors corresponding to their providers. In order to differentiate the models from different LLM provides during Multiple LLM Evaluations.]
+
+      **<u>Supported Sources:</u>**
+      - URL
+      - YouTube
+      - PDF
+      - DOCX
+
+      **<u>Supported LLMs:</u>**
+      - **Gemini**
+          - gemini-1.0-pro
+          - gemini-pro
+          - gemini-1.5-pro-latest
+      - **OpenAI**
+          - gpt-3.5-turbo
+          - gpt-4
+          - gpt-4-turbo
+          - gpt-3.5-turbo-16k
+      - **Azure OpenAI**
+          - gpt-35-turbo
+          - gpt-4
+          - gpt-35-turbo-16k
+      - **Anthropic**
+          - claude-3-5-sonnet-20240620
+          - claude-3-haiku-20240307
+          - claude-3-sonnet-20240229
+          - claude-3-opus-20240229
+      - **Groq**
+          - mixtral-8x7b-32768
+          - gemma2-9b-it
+          - llama-3.1-8b-instant
+          - llama3-70b-8192
+          - llama3-8b-8192
+          - llama3-groq-70b-8192-tool-use-preview
+          - llama3-groq-8b-8192-tool-use-preview
+      - **Hugging Face**
+          - huggingfaceh4/zephyr-7b-alpha
+          - huggingfaceh4/zephyr-7b-beta
+
+      **<u>Evaluation Metrics:</u>**
+      - Context Relevancy
+      - Answer Relevancy
+      - Groundedness
+
+      This release enhances the app's functionality by allowing simultaneous evaluations across multiple LLMs, providing a more comprehensive analysis. Future updates will continue to focus on improving the user experience and adding new features.
+
+      **Full Changelog**: https://github.com/ritwickbhargav80/quick-llm-model-evaluations/commits/v1.1.0
     """
-        <style>
-            .block-container { padding-top: 2.5rem; padding-bottom: 1rem; padding-left: 1rem; padding-right: 1rem; }
-            span[data-testid="stHeaderActionElements"] { display:none; }
-            [data-testid="stMetricLabel"] > div { text-align: center; }
-            iframe { display: none; }
-            .element-container > .stMarkdown > div[data-testid="stCaptionContainer"] { text-align: center; }
-        </style>
-    """,
-    unsafe_allow_html=True,
-)
+},
+    {
+        "version_name": "v1.0.0",
+        "header": "Single LLM Model Evaluation with BeyondLLM Latest",
+        "release_date": "August 28, 2024",
+        "markdown_content": """
+          This release introduces the initial version of our Streamlit app for single-model evaluations using beyondllm. Users need to provide input such as source related information, and credentials for large language models (LLMs). The app also supports Retrieval-Augmented Generation (RAG) and provides comprehensive evaluation metrics.
 
-# Button disabled state based on input validation and missing credentials
-disabled = (
-        api_key.strip() != "" and
-        ((bool(re.match("https:\/\/(.+)\.openai\.azure\.com[/]{0,1}", azure_oai_endpoint)) and (str(azure_oai_deployment_name).strip() != "") and (str(embeddings_oai_model_name).strip() != "")) if llm_selected=="Azure OpenAI" else True)
-    )
-try:
-    disabled = not (disabled and ((url.strip() != "") and (validators.url(url))))
-except NameError:
-    disabled = not (disabled and (uploaded_file is not None))
-if not disabled:
-    os.environ["API_KEY"] = api_key
-else:
-    st.error("Make sure your have done all the configurations before querying!")
+            **<u>Supported Sources:</u>**
+            - URL
+            - YouTube
+            - PDF
+            - DOCX
 
-# Question input with dynamic label based on source selection
-question = st.text_input(
-    label=f"Ask {llm_selected} a question from {'your youtube video' if(source_selected.lower() == 'youtube') else 'your document'}:",
-    value=(
-        st.secrets["DEFAULTS"]["PAGE_QUERY"]
-        if (source_selected.lower() == "url")
-        else (st.secrets["DEFAULTS"]["YOUTUBE_QUERY"] if (source_selected.lower() == "youtube") else "")
-    ),
-    disabled=disabled,
-)
+            **<u>Supported LLMs:</u>**
+            - Gemini
+                - gemini-1.0-pro
+                - gemini-pro
+                - gemini-1.5-pro-latest
+            - OpenAI
+                - gpt-3.5-turbo
+                - gpt-4
+                - gpt-4-turbo
+            - Azure OpenAI
+                - gpt-35-turbo
+                - gpt-35-turbo-16k
+                - gpt-4
+            - Anthropic
+                - claude-3-sonnet-20240229
+                - claude-3-haiku-20240307
+                - claude-3-opus-20240229
+                - claude-3-5-sonnet-20240620
 
-# Run query button disabled based on input validation or missing credentials or missing question
-to_run = st.button("Run query", disabled=(disabled or (question.strip() == "")))
+            **<u>Evaluation Metrics:</u>**
+            - Context Relevancy
+            - Answer Relevancy
+            - Groundedness
 
-try:
-    if question.strip() and to_run:
-        # ---------------------- EXTERNAL DATA LOAD ----------------------
-        msg = st.toast("Loading external data...", icon="⚙️"); time.sleep(1)
+            This release marks the first stable version of the app, tested for a smooth user experience across different models and sources. Future updates will include extended features and further optimizations.
 
-        if(source_selected.lower() in ("pdf", "docx")):
-            # Define the path to save uploaded files
-            save_path = "./uploaded_files"
+            **Full Changelog**: https://github.com/ritwickbhargav80/quick-llm-model-evaluations/commits/v1.0.0
+        """
+    }
+]
 
-            # Create the directory if it doesn't exist
-            if not os.path.exists(save_path):
-                os.makedirs(save_path)
-
-            # Combine the save path and uploaded file name to create the complete URL
-            url = os.path.join(save_path, uploaded_file.name)
-
-            # Open the file in binary write mode for safe storage of various file types
-            with open(url, "wb") as f:
-                f.write(uploaded_file.getbuffer())
-
-        data = source.fit(url, dtype=str(source_selected).lower(), chunk_size=512, chunk_overlap=0)
-
-        msg.toast("External data loaded!", icon="✅"); time.sleep(1)
-        msg.toast("Configuring embedding and LLM model...", icon="🔢"); time.sleep(1)
-        
-        # ---------------------- LLM MODEL CONFIGURATION ----------------------
-        # LLM model selection and configuration based on chosen provider
-        if llm_selected == "Gemini":
-            # uses 'models/embedding-001' by default for embeddings
-            embed_model = embeddings.GeminiEmbeddings(api_key=os.environ["API_KEY"])
-            llm_model = llms.GeminiModel(model_name=model_name, google_api_key=os.environ["API_KEY"])
-        elif llm_selected == "OpenAI":
-            # uses 'text-embedding-3-small' by default for embeddings
-            embed_model = embeddings.OpenAIEmbeddings(api_key=os.environ["API_KEY"])
-            llm_model = llms.ChatOpenAIModel(model=model_name, api_key=os.environ["API_KEY"])
-        elif llm_selected == "Azure OpenAI":
-            embed_model = embeddings.AzureAIEmbeddings(deployment_name=embeddings_oai_model_name, azure_key=os.environ["API_KEY"], endpoint_url=azure_oai_endpoint)
-            llm_model = llms.AzureOpenAIModel(model=model_name, azure_key = os.environ["API_KEY"], deployment_name=azure_oai_deployment_name, endpoint_url=azure_oai_endpoint)
-        elif llm_selected == "Anthropic":
-            # uses 'BAAI/bge-small-en-v1.5' by default for embeddings
-            embed_model = embeddings.FastEmbedEmbeddings()
-            llm_model = llms.ClaudeModel(api_key=os.environ["API_KEY"], model=model_name)
-        
-        msg.toast("Embedding and LLM model configured!", icon="👍"); time.sleep(1)
-
-        # ---------------------- RETRIEVE DOCUMENTS AND RUNNING GENERATOR MODEL ----------------------
-        msg.toast("Generating response by leveraging retriever and LLM...", icon="🏗️"); time.sleep(1)
-        
-        retriever = retrieve.auto_retriever(data, type="normal", top_k=3, embed_model=embed_model)
-        pipeline = generator.Generate(question=question, retriever=retriever, llm=llm_model)
-        
-        final_rag_output = pipeline.call()
-        
-        # ---------------------- SUMMARIZED RESPONSE DISPLAY IN CONTAINER ----------------------
-        st.container(border=True).write(f"**Response:**  \n   \n {final_rag_output}")
-        
-        msg.toast("Response generated!", icon="🥳"); time.sleep(1)
-
-        # ---------------------- EVALUATION SCORES ----------------------
-        msg.toast("Assessing response on evaluation benchmarks...", icon="📊"); time.sleep(1)
-        
-        cr_col, ar_col, grnd_col = st.columns(3)
-        # Format the retrieved evaluation scores
-        formatted_evals = format_rag_triad_evals(pipeline.get_rag_triad_evals())
-
-        # Displaying evaluation scores as metric
-        cr_col.metric(label="**Context Relevancy**", value=f"{formatted_evals['context_relevancy']}", help="This score between 0 (least relevant) to 10 (most relevant) indicates how well the system finds information related to your query.")
-        ar_col.metric(label="**Answer Relevance**", value=f"{formatted_evals['answer_relevancy']}", help="This score between 0 (completely irrelevant) to 10 (highly relevant) indicates how well the LLM's response aligns with your question.")
-        grnd_col.metric(label="**Groundedness**", value=f"{formatted_evals['groundness']}", help="This score between 0 (completely hallucinated) to 10 (fully grounded) determines the extent to which the LLM's responses are grounded.")
-
-        if(formatted_evals):
-            st.caption(":orange[**Disclaimer:** These metrics are generated with the use of the same LLM and the detailed prompts defined by the team of beyondllm. Hence, the response may not be fully accurate. Please use your judgment and consult original source for verification.]")
-
-        # Applying custom CSS for formatting metrics
-        metric_custom_css()
-
-        msg.toast("Evaluation completed successfully!", icon="🖨️"); time.sleep(1)
-
-except Exception as err:
-    # Error handling for API key related issues for supported models
-    if ("400 API key not valid." in err.__str__()) or ("Incorrect API key provided" in err.__str__()):
-        st.error("Check your credentials!")
-    else:
-        st.write("An error occurred:", err)
-
-    msg.toast("Uh-ohh! Something went wrong...", icon="☹️"); time.sleep(1)
+for idx, version in enumerate(versions):
+  expander = st.expander(f"{version['version_name']}{' (Latest)' if idx == 0 else ''}")
+  expander.subheader(version["header"])
+  expander.write(f"Release date: {version['release_date']}")
+  expander.markdown(version["markdown_content"], unsafe_allow_html=True)
